e0dbe43c91b1349c3364653d5fec4366bc6dd5a5
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2023-10-03 20:33:47 +0200

    fix docker build + add go-git

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+
+	git "github.com/go-git/go-git/v5"
 )
 
 const GH_ACTIONS_INPUT_PREF = "INPUT_"
@@ -15,4 +17,12 @@ func main() {
 
 	fmt.Printf("Input1: %s\n", input1)
 	fmt.Printf("Input2: %s\n", input2)
+
+	r, err := git.PlainOpen("./")
+	if err != nil {
+		panic(err)
+	}
+
+	h, err := r.Head()
+	fmt.Println(h.Name())
 }
